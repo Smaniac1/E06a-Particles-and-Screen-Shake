a455adff7c0313ca0e19bb0d17e8794f93b9a49e
Smaniac1 <Smaniac1@users.noreply.github.com>
2020-02-21 15:20:35 -0500

    Summary
    For this program I made three particle effects that are supposed to look like a camp fire, rain, and fog/clouds and also added screen shake when pressing the WASD keys.

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -6,4 +6,12 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("Up"):
+		$Camera.add_trauma(0.1)
+	if Input.is_action_just_pressed("Down"):
+		$Camera.add_trauma(0.1)
+	if Input.is_action_just_pressed("Left"):
+		$Camera.add_trauma(0.1)
+	if Input.is_action_just_pressed("Right"):
+		$Camera.add_trauma(0.1)
 	pass
